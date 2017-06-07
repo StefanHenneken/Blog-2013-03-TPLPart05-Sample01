63692b2d68e23309e11eda21c5072143bdfea577
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 21:57:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Sample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            Console.WriteLine("Start Run");
+            try
+            {
+                Thread t = new Thread(Do);
+                t.IsBackground = true;
+                t.Start();
+                t.Join();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Run: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End Run");
+                Console.ReadLine();
+            }            
+        }
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Unhandled Exception: " + ((Exception)e.ExceptionObject).Message);
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+        static public void Do()
+        {
+            Console.WriteLine("Start Do");
+            Thread.Sleep(1000);
+            int a = 1;
+            a = a / --a;
+            Console.WriteLine("End Do");
+        }
+    }
+}
